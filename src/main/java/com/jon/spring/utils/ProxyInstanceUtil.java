6b1cc226d50ad7546b7a1bae0f236e1c7d950664
Maolin Yu <maolin@michaels.com>
2023-10-09 14:02:43 +0800

    增加获取代理对象中的真实类

---
+++ src/main/java/com/jon/spring/utils/ProxyInstanceUtil.java
@@ -0,0 +1,42 @@
+package com.jon.spring.utils;
+
+import org.springframework.aop.framework.AdvisedSupport;
+import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.support.AopUtils;
+
+import java.lang.reflect.Field;
+
+/**
+ * 代理对象工具类
+ * 
+ * @author Maolin Yu
+ */
+public class ProxyInstanceUtil {
+
+    public static Object getTarget(Object proxy) throws Exception {
+        if (AopUtils.isJdkDynamicProxy(proxy)) {
+            return getJdkDynamicProxyTargetObject(proxy);
+        } else if (AopUtils.isCglibProxy(proxy)) {
+            return getCglibProxyTargetObject(proxy);
+        }
+        return proxy;
+    }
+
+    private static Object getCglibProxyTargetObject(Object proxy) throws Exception {
+        Field h = proxy.getClass().getDeclaredField("CGLIB$CALLBACK_0");
+        h.setAccessible(true);
+        Object dynamicAdvisedInterceptor = h.get(proxy);
+        Field advised = dynamicAdvisedInterceptor.getClass().getDeclaredField("advised");
+        advised.setAccessible(true);
+        return ((AdvisedSupport)advised.get(dynamicAdvisedInterceptor)).getTargetSource().getTarget();
+    }
+
+    private static Object getJdkDynamicProxyTargetObject(Object proxy) throws Exception {
+        Field h = proxy.getClass().getSuperclass().getDeclaredField("h");
+        h.setAccessible(true);
+        AopProxy aopProxy = (AopProxy)h.get(proxy);
+        Field advised = aopProxy.getClass().getDeclaredField("advised");
+        advised.setAccessible(true);
+        return ((AdvisedSupport)advised.get(aopProxy)).getTargetSource().getTarget();
+    }
+}
